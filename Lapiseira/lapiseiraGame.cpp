8637db02951b7ac1c27f881161e2654b1aae6e67
FrBreno <fbreno77@alu.ufc.br>
2021-10-29 18:13:23 -0300

    Update3.0

--- Lapiseira/lapiseiraGame.cpp
+++ Lapiseira/lapiseiraGame.cpp
@@ -34,9 +34,9 @@ struct GRAFITE
     return 0;
   }
 
-  std::string toString()
+  std::string toString(int cont)
   {
-    std::string aux = "Grafite: [" + std::to_string(this->calibre) + " : " + dureza + " : " + std::to_string(this->tamanho) + "]";
+    std::string aux = "Grafite " + std::to_string(cont) + ": [" + std::to_string(this->calibre) + " : " + dureza + " : " + std::to_string(this->tamanho) + "]";
     return aux;
   }
 };
@@ -44,7 +44,8 @@ struct GRAFITE
 struct LAPISEIRA
 {
   float calibre; // Em mm.
-  GRAFITE *grafite = nullptr;
+  // GRAFITE *grafite = nullptr;
+  std::vector<GRAFITE> grafites;
 
   LAPISEIRA(float calibre = 0.3f) : calibre{calibre}
   {
@@ -52,25 +53,31 @@ struct LAPISEIRA
 
   bool inserir(GRAFITE *grafite)
   {
+    int soma{0};
+
     if (grafite->calibre != this->calibre)
     {
       std::cout << "ERROR! Calibre incompativel\n";
-      this->grafite = nullptr;
       return false;
     }
-    if (this->grafite != nullptr)
+    for (int i{0}; i < (int)this->grafites.size(); i++)
     {
-      std::cout << "ERROR! Ja existe grafite\n";
+      soma += this->grafites[i].tamanho;
+    }
+    if ((soma + grafite->tamanho) > 150)
+    {
+      int falta = 150 - soma;
+      std::cout << "ERROR! Lapiseira cheia. Nao coloque um grafite maior que " << falta << std::endl;
       return false;
     }
-    this->grafite = grafite;
+    grafites.push_back(*grafite);
     return true;
   }
 
   GRAFITE *remover()
   {
-    GRAFITE *cis = this->grafite;
-    this->grafite = nullptr;
+    GRAFITE *cis = &this->grafites[(int)this->grafites.size() - 1];
+    this->grafites.pop_back();
     return cis;
   }
 
@@ -78,7 +85,7 @@ struct LAPISEIRA
   {
     int follhasEscritas{0};
 
-    if (this->grafite == nullptr)
+    if ((int)this->grafites.size() == 0)
     {
       std::cout << "ERROR! Sem grafite na lapiseira\n";
       return false;
@@ -86,27 +93,59 @@ struct LAPISEIRA
 
     while (folhas > 0)
     {
-      int desgaste = grafite->desgastePorFolha();
+      int desgaste = this->grafites[0].desgastePorFolha();
+      bool trocaGrafite{false};
 
-      if (this->grafite->tamanho == 0 || desgaste > this->grafite->tamanho)
+      if (this->grafites[0].tamanho == 0 || desgaste > this->grafites[0].tamanho)
       {
+        if (desgaste > this->grafites[0].tamanho && (int)this->grafites.size() > 1)
+        {
+          trocaGrafite = true;
+          int diferenca = desgaste - this->grafites[0].tamanho;
+          this->grafites[0].tamanho = 0;
+          for (int i{1}; i < (int)this->grafites.size(); i++)
+          {
+            int aux = diferenca;
+            diferenca -= this->grafites[i].tamanho;
+            this->grafites[i].tamanho -= aux;
+            if (this->grafites[i].tamanho > 0)
+            {
+              break;
+            }
+            else
+            {
+              this->grafites.erase(this->grafites.begin() + i);
+            }
+          }
+        }
+        if ((int)this->grafites.size() == 1)
+        {
+          std::cout << "\nTexto incompleto!\n";
+          std::cout << follhasEscritas << " folhas foram escritas\n";
 
-        std::cout << "\nTexto incompleto!\n";
-        std::cout << follhasEscritas << " folhas foram escritas\n";
-
-        std::cout << "\nCabou o grafite :/\n";
-        remover();
-        return false;
+          std::cout << "\nCabou o grafite :/\n";
+        }
+        this->grafites.erase(this->grafites.begin());
+        if ((int)this->grafites.size() == 0)
+        {
+          return false;
+        }
       }
-      grafite->tamanho -= desgaste;
+      if (!trocaGrafite)
+      {
+        this->grafites[0].tamanho -= desgaste;
+      }
       follhasEscritas++;
       folhas--;
     }
 
-    if (folhas == 0 && this->grafite->tamanho == 0)
+    if (folhas == 0 && this->grafites[0].tamanho == 0)
     {
-      std::cout << "\nCabou o grafite :/\n";
-      remover();
+      this->grafites.erase(this->grafites.begin());
+      if ((int)this->grafites.size() == 0)
+      {
+        std::cout << "\nCabou o grafite :/\n";
+      }
       return false;
     }
 
@@ -148,13 +187,16 @@ int main()
     else if (comando == "show")
     {
       std::cout << bic.toString();
-      if (bic.grafite == nullptr)
+      if ((int)bic.grafites.size() == 0)
       {
         std::cout << "Grafite: null\n";
       }
       else
       {
-        std::cout << bic.grafite->toString() << std::endl;
+        for (int i{0}; i < (int)bic.grafites.size(); i++)
+        {
+          std::cout << bic.grafites[i].toString(i + 1) << std::endl;
+        }
       }
     }
     else if (comando == "inserir")
